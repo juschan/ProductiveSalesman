440fffe262f9ef69ea21b05842e7dc7728296cd7
Justin Chan <justinchanuk@gmail.com>
2019-06-17 12:42:24 +0800

    Moved index.html to templates folder. Adjusted app to serve index.html as template

--- application.py
+++ application.py
@@ -1,6 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/hello")
 def hello():
     return "Hello World!"
+
+
+@app.route("/")
+def root():
+    return render_template('index.html')
